9664f806e20c46d2ccbc72d40a8d90e6e4a81825
jpwiig <jpwiig@gmail.com>
2024-08-30 12:56:08 +0200

    some boilerplate test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ExceptionHandlingH24
+
+This Github repository is meant as a demo for exception handling during the fall 24 pratice at OsloMet. You are free to do whatever you want with the code, but this is mainly for teaching purpouses. 
+
+Structure of this repository
+
+The main branch will be updated with the finished code **AFTER** the final lab with the finished code. 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 //we do everything in the main method
+        int writeAnumb = 0;
+//this is not the final demo!
+        try {
+            System.out.println("Please write a number:");
+            Scanner sc = new Scanner(System.in);
+
+            writeAnumb = sc.nextInt();
+            System.out.println("Numb" + writeAnumb);
+        }
+        catch (InputMismatchException e){
+            System.out.println("you should write a Number!");
+        }
+        catch (Exception e) {
+            System.out.println("error: " + e);
+        }
     }
 }
