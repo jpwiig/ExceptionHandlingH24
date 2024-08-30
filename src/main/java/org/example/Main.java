26b53f6b8743c79eef0b83d172d16bec0af72598
jpwiig <jpwiig@gmail.com>
2024-08-30 09:26:17 +0200

    added sample code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//we do everything in the main method
+    }
+}
