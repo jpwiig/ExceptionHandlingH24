df793b11df28db83bfec1eb15ecf00e00377dfe4
jpwiig <jpwiig@gmail.com>
2024-08-30 13:03:10 +0200

    removed wierd intelij code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ package org.example;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 //we do everything in the main method
